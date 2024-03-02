1ea2ee2d5695fb28ff27e619ff13648d870f3960
saurav <saurav.sagar@tothenew.com>
2024-03-02 23:55:33 +0530

    Updating Extent report Pojo class

--- src/main/java/utils/ExtentManager.java
+++ src/main/java/utils/ExtentManager.java
@@ -1,17 +1,37 @@
 package utils;
 
 public class ExtentManager {
-    public static String testDescription;
-    public static String executedQuery;
-    public static String executedQueryResult;
+    private static ThreadLocal<String> testDescription = new ThreadLocal<>();
+    private static ThreadLocal<String> executedQuery = new ThreadLocal<>();
+    private static ThreadLocal<String> executedQueryResult = new ThreadLocal<>();
 
-    public static String getTestDescription(){
-        return testDescription;
+    public static String getTestDescription() {
+        return testDescription.get();
     }
+
+    public static void setTestDescription(String description) {
+        testDescription.set(description);
+    }
+
     public static String getExecutedQuery() {
-        return executedQuery;
+        return executedQuery.get();
     }
+
+    public static void setExecutedQuery(String query) {
+        executedQuery.set(query);
+    }
+
     public static String getExecutedQueryResult() {
-        return executedQueryResult;
+        return executedQueryResult.get();
+    }
+
+    public static void setExecutedQueryResult(String result) {
+        executedQueryResult.set(result);
+    }
+
+    public static void clearAll() {
+        testDescription.remove();
+        executedQuery.remove();
+        executedQueryResult.remove();
     }
 }
